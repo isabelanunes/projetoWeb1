e07bcfbefe562488ea68dfcdae32077dbc3c079e
Alvaro Pedroso Queiroz <alvaropq1@hotmail.com>
2021-05-15 14:41:30 -0300

    Arrumando cors

--- app.js
+++ app.js
@@ -12,20 +12,7 @@ var app = express();
 let cors = require("cors");
 const bodyParser = require("body-parser");
 
-app.use((req, res, next) => {
-  //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-  res.header("Access-Control-Allow-Origin", "*");
-  //Quais são os métodos que a conexão pode realizar na API
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-
-  app.use(cors());
-  next();
-});
-
+app.use(cors());
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
