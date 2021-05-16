3b91789378c4b7cace6be30be9839c32430f283d
Isabela Nunes Caetano <isabela.caetano2@etec.sp.gov.br>
2021-05-16 15:30:55 -0300

    ajuste cors

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 let cors = require("cors");
 const bodyParser = require("body-parser");
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
